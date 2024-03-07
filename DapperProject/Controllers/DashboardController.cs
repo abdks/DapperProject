c94d2cbffb7a46ea9cb0e3ec07fc25fadd440347
AbdullahKus <62968246+abdks@users.noreply.github.com>
2024-03-07 19:43:44 +0300

    ......

--- DapperProject/Controllers/DashboardController.cs
+++ DapperProject/Controllers/DashboardController.cs
@@ -16,14 +16,25 @@ namespace DapperProject.Controllers
 
         public async Task<IActionResult> Index()
         {
-            // SQL sorgusunu kullanarak en çok deprem olan şehiri bul
+            // SQL sorgusunu kullanarak en az deprem olan şehiri bul
             string query = "SELECT TOP 1 province FROM EQAfad GROUP BY province ORDER BY COUNT(*) DESC";
             var connection = _context.CreateConnection();
             var mostAffectedCity = await connection.QueryFirstOrDefaultAsync<string>(query);
-
+            // SQL sorgusunu kullanarak en çok deprem olan şehiri bul
             string query2 = "SELECT TOP 1 province FROM EQAfad GROUP BY province ORDER BY COUNT(*) ASC";
             var connection2 = _context.CreateConnection();
             var mostAffectedCity2 = await connection2.QueryFirstOrDefaultAsync<string>(query2);
+            // 2023 yılındaki en yüksek depremin bilgilerini getir
+            string query3 = "SELECT TOP 1 province, magnitude FROM EQAfad WHERE YEAR(date) = 2023 ORDER BY magnitude DESC";
+            var connection3 = _context.CreateConnection();
+            var highestMagnitudeEarthquake = await connection3.QueryFirstOrDefaultAsync<dynamic>(query3);
+
+            // Viewbage'e şehir adını ve derecesini aktar
+            ViewBag.HighestMagnitudeCity = highestMagnitudeEarthquake?.province;
+            ViewBag.HighestMagnitude = highestMagnitudeEarthquake?.magnitude;
+
+
+
 
             // Viewbage'e şehir adını aktar
             ViewBag.MostAffectedCity = mostAffectedCity;
